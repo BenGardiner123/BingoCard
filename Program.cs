559764b3990f688680dea799f44da12fa0281f44
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-05-23 17:29:00 +1000

    beginning of bingo card

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace BingoCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+
+    class Menu {
+
+        public string option1;
+        public string option2;
+        public string option3;
+
+            
+    ///======================== constructor
+
+    public Menu()
+    {
+        
+    }
+
+     public Menu(string menuType)
+    {
+        /// i thinkthere will be a different typeof nmenu avauiable here
+
+
+    }
+    
+
+    }
+
+    class Card {
+
+        public int min
+        public int max
+
+        /// something for the grid framework herer
+
+
+        public Card()
+        {
+            
+        }
+
+        public Card(int min, int max)
+        {
+            /// Card creation ()
+            /// when you create a card it will the create a table absed on the min anbd max numbers
+            ///so regardless of the start number the framework should be the same  = a grid 
+
+
+
+            /// The card will be an array with the top of colum B is length of col A +1 - when they pick  to cross off we have to find the nu,ber at that position or search for th  nubmer and change it to an "x"
+
+            
+        }
+
+    }
+
+
+}
