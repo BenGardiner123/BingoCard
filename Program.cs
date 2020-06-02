9227b3e95b874d5f0d1ffd80810ead1a3ce84446
Ben Gardiner <bennygards@gmail.com>
2020-06-02 22:17:13 +1000

    added some sutff

--- Program.cs
+++ Program.cs
@@ -4,20 +4,16 @@ using System.Collections.Generic;
 namespace BingoCard
 {
     class Program
-            {
-                static void Main(string[] args)
-                {
-        
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.startMenu();
 
-                   /*  List<Card> PlayerCard = new List<Card>(25);
-                    Player p1 = new Player("Ben Gardiner", PlayerCard); */
-                    Card c1 = new Card(36, 1, 500);
-                    ////just did this to test the rand num generaotrand seems to be working!!!!!!!!!!!!!!!
-                 
-                    
-            
-                }
-            }    
+            /* List<Card> PlayerCard = new List<Card>(25);
+            Player p1 = new Player("Ben Gardiner", PlayerCard); */
+            //Card c1 = new Card(36, 1, 500);
+            ////just did this to test the rand num generaotrand seems to be working!!!!!!!!!!!!!!!
+        }
+    }    
 
-
-}
