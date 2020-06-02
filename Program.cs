16d4f18439ca82976bf3024aff16edf19c3a27ec
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-06-02 14:17:20 +1000

    added note to main

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace BingoCard
                    /*  List<Card> PlayerCard = new List<Card>(25);
                     Player p1 = new Player("Ben Gardiner", PlayerCard); */
                     Card c1 = new Card(36, 1, 500);
-                    
+                    ////just did this to test the rand num generaotrand seems to be working!!!!!!!!!!!!!!!
                  
                     
             
