7dddee5d056a619e648861ad56e609febee67d98
Ben Gardiner <bennygards@gmail.com>
2020-06-02 21:05:13 +1000

    added further work on game and card

--- Card.cs
+++ Card.cs
@@ -9,40 +9,43 @@ namespace BingoCard
         public int max;
         public List<int> CardNums;
         public int cardSize;
-
-        /* public static Random = new Random(int); */
-
-
+        Random rand = new System.Random();
 
         public Card(int totalNumreqd, int cardMin, int cardMax)
         {
-
+            //totalNumReqd 25, cardmin 1, cardmax 100
             this.min = cardMin;
             this.max = cardMax;
             this.cardSize = totalNumreqd;
-
+            this.CardNums = new List<int>();
 
             // ithink i need togenerate alist of random numbers say 1- 1000 andout them imhere everytime a card gets created i can iterate out whatever range is required making sure they dont repeat
             // here we create an instance of a random object - YAKEN FROM MY PREVIOUS C# WORKS
-            var rand = new System.Random();
-            List<int> firstlist = new List<int>();
+
             for (int i = cardMin; i < totalNumreqd + 1; i++)
-            {
-                // then i need to store the random number in somewhere object.Next(n, n)
-                var randCardNum = rand.Next(1,cardMax);
-                firstlist.Add(randCardNum);
+            {               
+                bool validNumber = false; // initialise escape variable 
+                var randCardNum = rand.Next(1, cardMax); // generate random number
+
+                while (validNumber == false) // check if number is valid
+                {                    
+                    if (!CardNums.Contains(randCardNum))  // check if number is not in list
+                    {
+                        validNumber = true; // not in list, hence we can update the validNumber to true so the loop won't runa gain
+                    }
+                    else
+                    {
+                        randCardNum = rand.Next(1, cardMax); // otherwise regenerate a new random number
+                    }
+                }
+
+                CardNums.Add(randCardNum);
             }
-            foreach (int number in firstlist)
-            {
-                Console.WriteLine(number);
-            }
-
-
-        }
-
-      
+        }      
     }
 }
+
+
 
 
 
--- Game.cs
+++ Game.cs
@@ -4,32 +4,29 @@ using System.Linq;
 
 namespace BingoCard
 {
-     public class Game
+    public class Game
     {
-        public string playerName;
-        
+        //public string playerName;
+        public Player player;
 
-        // attributes
-
-        ////..................------------....-----------........ CONSTRUCTOR.....
-        public Game(string playerName)
+        public Game()
         {
-            this.playerName = playerName;
+            //this.playerName = playerName;
         }
-
-
-        // methods ----------------------------------------------------------------
 
         public void startMenu()
         {
-            
+            player = new Player();
             Console.WriteLine("Welcome to Bingo");
             Console.WriteLine("Please enter your name ");
-            Console.WriteLine("Please enter your selcection");
+            player.Name = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + player.Name + ". Please enter your selection");
             Console.WriteLine("1. Create New Bingo Card");
             Console.WriteLine("2. Exit");
-            ///insert menu check here
-            var userinput = 1; Convert.ToInt32(Console.ReadLine());
+
+            int userinput = Convert.ToInt32(Console.ReadLine());
+
             if (userinput == 1)
             {
                 createCardMenu();
@@ -37,59 +34,100 @@ namespace BingoCard
             else if (userinput == 2)
             {
                 Console.Write("You have not entered a number. . .Goodbye");
-                startMenu();
+                return;
             }
+        }
 
+        public void createCardMenu()
+        {
+            Console.WriteLine("To create your card please enter the size of the board");
+            Console.WriteLine("the board size must be a sqaure number for the board to be created");
+            Console.WriteLine("For example 2x2 = 4, 3x3 = 9, 4x4 = 16, 5x5 = 25, etc...");
+            Console.WriteLine("The actual numbers on the card are seqential from 1 - max number available per card");
+            Console.WriteLine("Now please enter your board size");
+            int userBoardSize = Convert.ToInt32(Console.ReadLine());
+            var truth = isSqr(userBoardSize);
 
-            public void createCardMenu()
+            if (truth)
             {
+                Console.WriteLine("Please enter the smallest number in range");
+                int minRange = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the largest number in range");
+                int maxRange = Convert.ToInt32(Console.ReadLine());
 
+                player.AddCard(userBoardSize, minRange, maxRange);
+                ShowCardGrid(userBoardSize);
 
-                Console.WriteLine("To create your card please enter the size of the board");
-                Console.WriteLine("the board size must be a sqaure number for the board to be created");
-                Console.WriteLine("For example 2x2 = 4, 3x3 = 9, 4x4 = 16, 5x5 = 25, etc...");
-                Console.WriteLine("The actual numbers on the card are seqential from 1 - max number available per card");
-                Console.WriteLine("Now please enter your board size");
-                int userBoardSize = Convert.ToInt32(Console.ReadLine());
-                var truth = isSqr(userBoardSize);
+                // game has a player member with a card member which has all valid numbers in the range the user has inputted
 
-                if (truth)
+                
+            }
+            else
+            {
+                Console.WriteLine("Your number was not a sqaure");
+                createCardMenu();
+            }
+        }
+
+        public void ShowCardGrid(int gridsize)  // gridsize was 16
+        {
+            double squareRoot = Math.Sqrt(gridsize);     // 4
+
+            //player.card.cardNums is a list of all the numbers 
+
+            int index = 0;
+
+            for (int y = 0; y < squareRoot; y++)
+            {
+                bool isFirst = true;
+
+                for (int x = 0; x < squareRoot; x++)
                 {
-
+                    if (isFirst == true)
+                    {
+                        Console.WriteLine(player.card.CardNums[index]);
+                        isFirst = false;
+                    }
+                    else
+                    {
+                        Console.Write(" " + player.card.CardNums[index]);
+                    }
+                   
+                    index++;
                 }
-                else
-                {
-                    Console.WriteLine("Your number was not a sqaure");
-                    startMenu();
-
-                }
-
             }
 
-            public string viewCardMEnu()
-            {
 
-            }
+        }
 
-            public string MArkCardoffMenu()
-            {
+        //public void viewCardMEnu()
+        //{
 
-            }
-            static bool isSqr(int num)
-            {
-                // get the square root WITH any fractional component 
-                double sq1 = Math.Sqrt(num);
+        //}
 
-                // get square root with fraction truncated by casting as an int
-                int sq2 = (int)Math.Sqrt(num);
+        //public string MArkCardoffMenu()
+        //{
 
-                // compare the numbers as doubles and see if equal
-                // if the fraction part of sq1 was zero (perfect square), then 
-                // the int and the double will be equivalent
-                if (sq1 == (double)sq2)
-                    return true;
-                return false;
+        //}
 
-            }
+        public bool isSqr(int num)
+        {
+            // get the square root WITH any fractional component 
+            double sq1 = Math.Sqrt(num);
 
+            // get square root with fraction truncated by casting as an int
+            int sq2 = (int)Math.Sqrt(num);
+
+            // compare the numbers as doubles and see if equal
+            // if the fraction part of sq1 was zero (perfect square), then 
+            // the int and the double will be equivalent
+            if (sq1 == (double)sq2)
+                return true;
+            return false;
+
+        }
+
+    }
+
+}
  
